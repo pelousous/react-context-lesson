14d05ae1ef77f66488465c0818e9ea468e37ee90
davide-ravasi <info@davideravasi.com>
2021-06-08 10:43:18 +0200

    refs create and use context instead of redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 var config = {
-
+  apiKey: "AIzaSyCErWE-ZKe44ln5ag0Ibm8cxzcUmFW9pT4",
+  authDomain: "crwn-db-02.firebaseapp.com",
+  databaseURL: "https://crwn-db-02.firebaseio.com",
+  projectId: "crwn-db-02",
+  storageBucket: "crwn-db-02.appspot.com",
+  messagingSenderId: "206580718199",
+  appId: "1:206580718199:web:a32325f75f407b43c44472",
+  measurementId: "G-H0QK4F7R3D"
 };
 
 firebase.initializeApp(config);
