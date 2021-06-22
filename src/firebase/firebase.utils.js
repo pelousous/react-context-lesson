1b0fde1c9fda345a7d327a87692688ec7700c252
davide-ravasi <info@davideravasi.com>
2021-06-22 10:42:43 +0200

    added styled component to cart-dropdown custom-button

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,10 +1,8 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
-var config = {
-
-};
+var config = {};
 
 firebase.initializeApp(config);
 
@@ -23,10 +21,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
@@ -37,7 +35,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
