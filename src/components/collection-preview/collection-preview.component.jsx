622f8befbfcba0edfa8149939ca33a85d3526485
davide-ravasi <info@davideravasi.com>
2021-06-29 18:01:14 +0200

    added collection-preview and collections-overview to styled component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import CollectionItem from '../collection-item/collection-item.component';
+import CollectionItem from "../collection-item/collection-item.component";
 
-import './collection-preview.styles.scss';
+//import "./collection-preview.styles.scss";
+
+import {
+  CollectionPreviewContainer,
+  CollectionPreviewTitle,
+  CollectionPreviewList,
+} from "./collection-preview.styles";
 
 const CollectionPreview = ({ title, items }) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
-    <div className='preview'>
+  <CollectionPreviewContainer>
+    <CollectionPreviewTitle>{title.toUpperCase()}</CollectionPreviewTitle>
+    <CollectionPreviewList>
       {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
+        .map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-    </div>
-  </div>
+    </CollectionPreviewList>
+  </CollectionPreviewContainer>
 );
 
 export default CollectionPreview;
