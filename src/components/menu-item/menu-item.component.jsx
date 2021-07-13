656877c5a61a68dfe8936985193a6695bd8f95fe
davide-ravasi <info@davideravasi.com>
2021-07-13 16:10:20 +0200

    added menuitem and header styled components

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { withRouter } from "react-router-dom";
 
-import './menu-item.styles.scss';
+import "./menu-item.styles.scss";
+
+import {
+  MenuItemContainer,
+  MenuItemBgImage,
+  MenuItemContent,
+  MenuItemTitle,
+  MenuItemSubtitle,
+} from "./menu-item.styles";
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-  <div
-    className={`${size} menu-item`}
+  <MenuItemContainer
+    size={size}
     onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
-    <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-    />
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>
+    <MenuItemBgImage backgroundImage={imageUrl} />
+    <MenuItemContent>
+      <MenuItemTitle>{title.toUpperCase()}</MenuItemTitle>
+      <MenuItemSubtitle>SHOP NOW</MenuItemSubtitle>
+    </MenuItemContent>
+  </MenuItemContainer>
 );
 
 export default withRouter(MenuItem);
