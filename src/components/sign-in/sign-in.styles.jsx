76c5a3e42aad8ccd375ea314e181c2ad6821a196
davide-ravasi <info@davideravasi.com>
2021-07-15 17:32:56 +0200

    added styled components to sign-in

---
+++ src/components/sign-in/sign-in.styles.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+export const SignInContainer = styled.div`
+  width: 380px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SignInTitle = styled.h1`
+  margin: 10px 0;
+`;
+
+export const SignInButtonsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
